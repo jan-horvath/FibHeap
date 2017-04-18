1813ccd43092a416cdcdbb5cb20f1956cf7cbbbf
Ján Horváth <xhorvat6@fi.muni.cz>
2017-04-18 13:57:05 +0200

    added constructors and operator declarations

--- FibHeap.hpp
+++ FibHeap.hpp
@@ -21,7 +21,7 @@ public:
 		unsigned m_degree;
 		Value m_key;
 
-		Node(const Value &value);
+		Node(const Value &);
 
 		friend class FibHeap;
 	};
@@ -41,6 +41,25 @@ public:
 	//creates empty Fibonacci Heap
 	FibHeap(); //: m_top(nullptr), m_number(0) {}
 
+	//copy constructs Fibonacci Heap (deep copy)
+	FibHeap(const FibHeap&);
+
+	//move constructs Fibonacci Heap
+	FibHeap(FibHeap&&);
+
+	//copy assignment operator
+	FibHeap operator=(const FibHeap&);
+
+	//move assignment operator
+	FibHeap operator=(FibHeap &&);
+
+	//constructs Fibonacci Heap from range
+	template <typename It>
+	FibHeap(It begin, It end);
+
+	//constructs Fibonacci Heap from initializer list
+	FibHeap(std::initializer_list<Value>);
+
 	//Returns top value of Fibonacci Heap
 	const Value* top() const;
 
@@ -52,12 +71,12 @@ public:
 
 	//inserts new value into Fibonacci Heap
 	//returns handler for this value
-	Handler insert(const Value& value);
+	Handler insert(const Value&);
 
 	//unites current heap with another one
 	//the other heap is invalidated
 	//current heap will contain all values
-	void uniteWith(FibHeap& other);
+	void uniteWith(FibHeap&);
 
 	//extracts top value
 	//this value is removed from the heap and new one is selected
@@ -67,14 +86,14 @@ public:
 	//deletes value pointed to by handler
 	//handler is supplied by the insert function
 	//returns true if deletion was successful
-	bool delete_value(Handler &handler);
+	bool delete_value(Handler &);
 
 	//decreases value of a key, pointed to by handler
 	//may cascade cut the heap
-	bool decrease_key(const Handler &handler);
+	bool decrease_key(const Handler &);
 
 	//swaps two different Fibonacci Heaps
-	void swap(FibHeap &other);
+	void swap(FibHeap &);
 
 private:
 	//modifies the heap so that it does not contain two trees with the same degree
--- test.cpp
+++ test.cpp
@@ -1,11 +1,31 @@
 #include "catch.hpp"
+#include "FibHeap.hpp"
 #include <iostream>
 
 #define CATCH_CONFIG_MAIN
 
-TEST_CASE( "testing testing" ) {
-  int i = 1;
-  i = i++;
-  REQUIRE(i == 1);
+TEST_CASE( "Basic default constructor test" ) {
+	FibHeap<int> testHeap;
+
+	REQUIRE(!testHeap.top());
+	REQUIRE(testHeap.empty());
+	REQUIRE(testHeap.size() == 0);
 }
+
+TEST_CASE( "Basic copy constructor test, empty heap" ) {
+	FibHeap<int> emptyHeap;
+	FibHeap<int> copiedHeap(emptyHeap);
+
+	REQUIRE(!copiedHeap.top());
+	REQUIRE(copiedHeap.empty());
+	REQUIRE(copiedHeap.size() == 0);
+
+	REQUIRE(copiedHeap.top() == emptyHeap.top());
+	REQUIRE(copiedHeap.empty() == emptyHeap.empty());
+	REQUIRE(copiedHeap.size() == emptyHeap.size());
+}
+
+
+
+
 
